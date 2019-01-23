618b1d92ec8fae178a77272eee805659ed216070
Donna <donnamcla@gmail.com>
2019-01-23 18:26:05 -0500

    [EDSP 519] changes for gh-pages

--- src/components/App.js
+++ src/components/App.js
@@ -8,11 +8,11 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <Route exact path={`${process.env.PUBLIC_URL}/`} component={Thesauri} />
+        <Route exact path={`/`} component={Thesauri} />
 
-        <Route exact path={`${process.env.PUBLIC_URL}/resultsList`} render={(props) => <ResultsList {...props} API_KEY={this.props.API_KEY} />} />
+        <Route exact path={`/resultsList`} render={(props) => <ResultsList {...props} API_KEY={this.props.API_KEY} />} />
 
-        <Route exact path={`${process.env.PUBLIC_URL}/id/:id`} render={(props) => <TermInfo {...props} API_KEY={this.props.API_KEY} location={props.location} />} />
+        <Route exact path={`/id/:id`} render={(props) => <TermInfo {...props} API_KEY={this.props.API_KEY} location={props.location} />} />
       </div>
     );
   }

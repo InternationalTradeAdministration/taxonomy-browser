43c6ce1d472bbf4fd1fd2b105fac6ea534512e1b
Donna <donnamcla@gmail.com>
2019-01-24 08:26:23 -0500

    [EDSP 519] add tests

--- src/taxonomy.test.js
+++ src/taxonomy.test.js
@@ -18,4 +18,59 @@ test('main title is present', async () => {
   expect(mainTitleText).toBe('Thesaurus of International Trade Terms');
 });
 
+test('user can search based on text input', async () => {
+  
+  // enter the work "banks" in the text input element, then click Search button
+  const textInput = '#taxonomy_container input[type="text"]';
+  const searchButton = '#taxonomy_container div.center button';
+  await page.click(textInput);
+  await page.type(textInput, "banks");
+  await page.click(searchButton);
+
+  // arrive on the Search Results page, click the first item
+  const searchResultsHeader = '#taxonomy_container h3';
+  await page.waitForSelector(searchResultsHeader);
+  const headerText = await page.$eval(searchResultsHeader, e => e.innerHTML);
+  expect(headerText).toBe('Search Results');
+  const firstResult = '#taxonomy_container > div > div > ul:nth-child(3) > li:nth-child(1) > a';
+  await page.waitForSelector(firstResult, 20000);
+  await page.click(firstResult);
+
+  // arrive on the TermInfo page, identify concept group
+  const termInfoLabel = '#taxonomy_container div.breadcrumb h1';
+  await page.waitForSelector(termInfoLabel);
+  const conceptGroup = '#taxonomy_container > div > div > div.superTerms > ul > li > a';
+  await page.waitForSelector(conceptGroup);
+  const ITA_home = '#taxonomy_container > div > div > div.breadcrumb > h4 > a';
+  await page.waitForSelector(ITA_home);
+  await page.click(ITA_home);
+});
+
+test('user can search based on a checkbox input', async () => {
+  
+  // click the "Industries" box, then click Search button
+  const IndustriesCheckbox = '#taxonomy_container input[type="checkbox"]:nth-child(2)';
+  const searchButton = '#taxonomy_container div.center button';
+  const searchResultsHeader = '#taxonomy_container h3';
+  await page.click(IndustriesCheckbox);
+  await page.click(searchButton);
+
+  // arrive on the Search Results page, click the first item
+  await page.waitForSelector(searchResultsHeader, 20000);
+  const headerText = await page.$eval(searchResultsHeader, e => e.innerHTML);
+  expect(headerText).toBe('Search Results');
+  const firstResult = '#taxonomy_container > div > div > ul:nth-child(3) > li:nth-child(1) > a';
+  await page.waitForSelector(firstResult, 20000);
+  await page.click(firstResult);
+  
+  // arrive on the TermInfo page, identify concept group
+  const termInfoLabel = '#taxonomy_container div.breadcrumb h1';
+  await page.waitForSelector(termInfoLabel);
+  const conceptGroup = '#taxonomy_container > div > div > div.superTerms > ul > li > a';
+  await page.waitForSelector(conceptGroup);
+  const ITA_home = '#taxonomy_container > div > div > div.breadcrumb > h4 > a';
+  await page.waitForSelector(ITA_home);
+  await page.click(ITA_home);
+});
+
 afterAll(() => browser.close());

27746e76f1679283f7bbddb580602a3bfaa7f20a
Donna <donnamcla@gmail.com>
2019-01-29 10:27:25 -0500

    Updates

---
+++ precache-manifest.59e5216d8b19b640d7f2d4810fcc2010.js
@@ -0,0 +1,14 @@
+self.__precacheManifest = [
+  {
+    "revision": "91a6c7ca04527506a3d9",
+    "url": "/taxonomy-browser/static/js/taxonomy-browser.js"
+  },
+  {
+    "revision": "91a6c7ca04527506a3d9",
+    "url": "/taxonomy-browser/static/css/taxonomy-browser.css"
+  },
+  {
+    "revision": "c2f576e23f366d151d04c04be3191fba",
+    "url": "/taxonomy-browser/index.html"
+  }
+];

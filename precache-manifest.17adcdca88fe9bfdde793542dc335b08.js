ee5ea00e432c3894b5410793937278f68c2fe376
Donna Hogan <ddhogan@users.noreply.github.com>
2019-06-25 21:57:34 -0400

    Updates

---
+++ precache-manifest.17adcdca88fe9bfdde793542dc335b08.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "d96a81116b47c3d157b6",
+    "url": "./static/css/taxonomy-browser.css"
+  },
+  {
+    "revision": "d96a81116b47c3d157b6",
+    "url": "./static/js/taxonomy-browser.js"
+  },
+  {
+    "revision": "0dadb5de3542be650dbb328e4ba45656",
+    "url": "./static/media/dropdown-icon.0dadb5de.svg"
+  },
+  {
+    "revision": "a6483469360136901caf4e7b4a4f379c",
+    "url": "./static/media/download.a6483469.svg"
+  },
+  {
+    "revision": "d8d309bbb8600a704d99ee33ccd76144",
+    "url": "./static/media/open_in_new.d8d309bb.svg"
+  },
+  {
+    "revision": "2eb85db0a040082a4ab4af861bc7e6e0",
+    "url": "./index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.699f135d1ae3d49f0dd3443adbf20344.js"
+  "./precache-manifest.17adcdca88fe9bfdde793542dc335b08.js"
 );
 
 workbox.clientsClaim();

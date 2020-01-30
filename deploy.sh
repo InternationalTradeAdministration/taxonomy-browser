335214d85b72bf4a5f2eece4e18b9b553f718796
Donna Hogan <ddhogan@users.noreply.github.com>
2020-01-30 14:46:17 -0500

    Dockerize and add scripts for deploying to ITA Azure Dev Tenant

---
+++ deploy.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+sudo az acr login --name dataservices
+sudo docker login dataservices.azurecr.io -u dataservices -p $AZURE_CONTAINER_KEY  #use container username and access key
+sudo docker build -t dataservices.azurecr.io/ita-taxonomy-browser .
+sudo docker push dataservices.azurecr.io/ita-taxonomy-browser
+
